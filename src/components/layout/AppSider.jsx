faee92f39b14c664f688f24e1cebf6efba86853a
Олег <OllegOzz@yandex.ru>
2024-02-28 13:45:58 +0300

    data + sider-template

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,17 +1,54 @@
 import React from 'react'
-import { Layout } from 'antd';
+import { Layout, Card, Statistic, List, Typography } from 'antd';
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+
 
 const siderStyle = {
-    textAlign: 'center',
-    lineHeight: '120px',
-    color: '#fff',
-    backgroundColor: '#1677ff',
+    padding: '1rem',
 };
+
+const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+];
 
 const AppSider = () => {
     return (
         <Layout.Sider width="25%" style={siderStyle}>
-            Sider
+            <Card style={{ marginBottom: '1rem' }}>
+                <Statistic
+                    title="Active"
+                    value={11.28}
+                    precision={2}
+                    valueStyle={{ color: '#3f8600' }}
+                    prefix={<ArrowUpOutlined />}
+                    suffix="%"
+                />
+                <List
+                    size='small'
+                    dataSource={data}
+                    renderItem={(item) => (
+                        <List.Item>
+                            <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                        </List.Item>
+                    )}
+                />
+            </Card>
+
+            <Card style={{ marginBottom: '1rem' }}>
+                <Statistic
+                    title="Idle"
+                    value={9.3}
+                    precision={2}
+                    valueStyle={{ color: '#cf1322' }}
+                    prefix={<ArrowDownOutlined />}
+                    suffix="%"
+                />
+            </Card>
+
         </Layout.Sider>
     )
 }
