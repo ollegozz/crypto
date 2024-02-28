2af90edd0f71602adcf4b94b1cc8c23d70ab84f6
Олег <OllegOzz@yandex.ru>
2024-02-28 10:18:21 +0300

    layout comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,18 @@
 import { Layout } from 'antd';
-const { Header, Footer, Sider, Content } = Layout;
-
-const headerStyle = {
-  textAlign: 'center',
-  color: '#fff',
-  height: 64,
-  paddingInline: 48,
-  lineHeight: '64px',
-  backgroundColor: '#4096ff',
-};
-
-const contentStyle = {
-  textAlign: 'center',
-  minHeight: 120,
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#0958d9',
-};
-
-const siderStyle = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#1677ff',
-};
-
-const footerStyle = {
-  textAlign: 'center',
-  color: '#fff',
-  backgroundColor: '#4096ff',
-};
-
-const layoutStyle = {
-  borderRadius: 8,
-  overflow: 'hidden',
-  width: 'calc(50% - 8px)',
-  maxWidth: 'calc(50% - 8px)',
-};
+import AppContent from './AppContent';
+import AppHeaader from './components/layout/AppHeaader';
+import AppSider from './components/layout/AppSider';
 
 function App() {
   return (
-    <Layout style={layoutStyle}>
-      <Header style={headerStyle}>Header</Header>
+    <Layout>
+      <AppHeaader />
       <Layout>
-        <Sider width="25%" style={siderStyle}>
-          Sider
-        </Sider>
-        <Content style={contentStyle}>Content</Content>
+        <AppSider />
+        <AppContent />
       </Layout>
-      <Footer style={footerStyle}>Footer</Footer>
     </Layout>
   )
-    
-  
 }
 
 export default App
