ec1d3da9d9dbfae152ff66dc38bd9c357030f3fa
Олег <OllegOzz@yandex.ru>
2024-02-28 10:03:42 +0300

    ant layout

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const headerStyle = {
   lineHeight: '64px',
   backgroundColor: '#4096ff',
 };
+
 const contentStyle = {
   textAlign: 'center',
   minHeight: 120,
@@ -16,17 +17,20 @@ const contentStyle = {
   color: '#fff',
   backgroundColor: '#0958d9',
 };
+
 const siderStyle = {
   textAlign: 'center',
   lineHeight: '120px',
   color: '#fff',
   backgroundColor: '#1677ff',
 };
+
 const footerStyle = {
   textAlign: 'center',
   color: '#fff',
   backgroundColor: '#4096ff',
 };
+
 const layoutStyle = {
   borderRadius: 8,
   overflow: 'hidden',
