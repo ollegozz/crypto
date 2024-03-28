23ab8f502c195bc3ed6685b05e7d3f7ed92f1da9
Олег <OllegOzz@yandex.ru>
2024-03-28 23:59:59 +0300

    1

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,10 +1,18 @@
-import { Layout } from 'antd';
 import AppHeader from './AppHeader';
 import AppSider from './AppSider';
 import AppContent from './AppContent';
+import { useContext } from 'react';
+import CryptoContext from '../../context/crypto-context';
+
+import { Layout, Spin } from 'antd';
 
 export default function AppLayout() {
-    
+    const {loading} = useContext(CryptoContext);
+
+    if (loading) {
+        return <Spin fullscreen />
+    }
+
     <Layout>
         <AppHeader />
         <Layout>
