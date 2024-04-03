62c980d5c93a766fda716fd38e3999ef704a2a98
Олег <OllegOzz@yandex.ru>
2024-04-03 19:00:56 +0300

    header

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,18 +1,73 @@
 import React from 'react'
-import { Layout } from 'antd';
+import { Layout, Select, Space, Button } from 'antd';
 
 const headerStyle = {
+    width: '100%',
     textAlign: 'center',
-    color: '#fff',
     height: 60,
-    paddingInline: 48,
-    lineHeight: '64px',
-    backgroundColor: '#4096ff',
+    padding: '1rem',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    // background: 'white'
 };
+
+const handleChange = (value) => {
+    console.log(`selected ${value}`);
+};
+const options = [
+    {
+        label: 'China',
+        value: 'china',
+        emoji: '🇨🇳',
+        desc: 'China (中国)',
+    },
+    {
+        label: 'USA',
+        value: 'usa',
+        emoji: '🇺🇸',
+        desc: 'USA (美国)',
+    },
+    {
+        label: 'Japan',
+        value: 'japan',
+        emoji: '🇯🇵',
+        desc: 'Japan (日本)',
+    },
+    {
+        label: 'Korea',
+        value: 'korea',
+        emoji: '🇰🇷',
+        desc: 'Korea (韩国)',
+    },
+];
 
 const AppHeader = () => {
     return (
-        <Layout.Header style={headerStyle}>Header</Layout.Header>
+        <Layout.Header style={headerStyle}>
+            <Select
+                style={{
+                    width: '250px',
+                }}
+                value= 'Press # to open'
+                defaultValue={['china']}
+                // onChange={handleChange}
+                optionLabelProp="label"
+                options={options}
+                optionRender={(option) => (
+                    <Space>
+                        {/* <span role="img" aria-label={option.data.label}>
+                            {option.data.emoji}
+                        </span>
+                        {option.data.desc} */}
+                        <img/> bitcoin
+                    </Space>
+                )}
+            />
+                <Button type="primary">Add asset</Button>
+                
+        </Layout.Header>
     )
 }
 
